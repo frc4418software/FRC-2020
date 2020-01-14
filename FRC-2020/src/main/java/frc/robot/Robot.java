4f9a5d41492132bfa0772ddb5d083c8b8f6c6ca3
lucas-davidson <55169723+lucas-davidson@users.noreply.github.com>
2020-01-13 21:03:32 -0700

    Update Robot.java

--- FRC-2020/src/main/java/frc/robot/Robot.java
+++ FRC-2020/src/main/java/frc/robot/Robot.java
@@ -8,25 +8,17 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.I2C;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
-
-import com.revrobotics.frc.ColorSensorV3;
+import frc.robot.subsystems.ColorSensorSubsystem;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  * */
-public class Robot extends TimedRobot {
-
-  private final I2C.Port i2cPort = I2c.Port.kOnboard;
-  
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-
+public class Robot extends TimedRobot  {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -56,10 +48,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-    final Color detectedColor = m_colorSensor.getColor();
-    SmartDashboard.putNumber("Red", detectedColor.red);
-    SmartDashboard.putNumber("Green", detectedColor.green);
-    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    ColorSensorSubsystem.DnDColors();
   }
 
   /**
