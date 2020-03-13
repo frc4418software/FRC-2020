f31c2556a5243f904447e603d86d4dd72e6f88d9
LeGrandOuiOui <aragorn.wang@s.stemk12.org>
2020-03-12 22:38:33 -0600

    fwefwef
    wefwefwef

--- FRC-2020/src/main/java/frc/robot/subsystems/ReceiveJevoisDataSubsys.java
+++ FRC-2020/src/main/java/frc/robot/subsystems/ReceiveJevoisDataSubsys.java
@@ -75,7 +75,7 @@ public class ReceiveJevoisDataSubsys extends SubsystemBase {
         try {
             // Create a new serial port for a USB connection from the JeVois
             jevois = new SerialPort(115200, SerialPort.Port.kUSB1);
-            SmartDashboard.putString("Step 1", "Created new JeVois serialport");
+            SmartDashboard.putString("JeVois Port", "Created new JeVois serialport");
           }
           catch (UncleanStatusException use) {
             SmartDashboard.putString("SerialPortError","UncleanStatusException: Make sure the serial port is being reset or cleaned without a proper delay!");
@@ -83,6 +83,8 @@ public class ReceiveJevoisDataSubsys extends SubsystemBase {
           catch (NullPointerException npe) {
             SmartDashboard.putString("SerialPortError","NullPointerException: Could not find the JeVois on the serial port, is it plugged into the top USB port?");
         }
+
+        jevois.enableTermination();
     }
     //================================================================
     // Sub-function
@@ -106,20 +108,25 @@ public class ReceiveJevoisDataSubsys extends SubsystemBase {
                 // Split the valid data (assumed that the data is x and y coords) into elements of a parsed string array
                 setParsedData(getJevoisString().split(getDelims()));
                 
+
             }
             catch (StringIndexOutOfBoundsException siobe) {
-                SmartDashboard.putString("ParseXYError","StringIndexOutOfBoundsException: Did not receive any actual data from the JeVois, check the streaming mode for the JeVois and the USB serial port connection");
+                SmartDashboard.putString("ParseXYError","StringIndexOutOfBoundsException: big not work");
+            }
+            
+            try {
+                // Seperately set the X and Y coords from the parsed string array
+                setXCoord(Integer.parseInt(getParsedData()[0]));
+                setYCoord(Integer.parseInt(getParsedData()[1]));
             }
             catch (NumberFormatException nfe) {
-                SmartDashboard.putString("ParseXYError", "NumberFormatException: Something wrong with the formatting of the JeVois string");
-            }
-            // TODO NumberFormatException happens here, make sure string isn't empty, find out what it is
-            // Seperately set the X and Y coords from the parsed string array
-            setXCoord(Integer.parseInt(getParsedData()[0]));
-            setYCoord(Integer.parseInt(getParsedData()[1]));
+                SmartDashboard.putString("ParseXYError", "NumberFormatException: big bad happen");
 
+                
+            }
+            
             // Status feature 2: Receiving X and Y coordinates from JeVois raw string
-            SmartDashboard.putString("Feature_2", "X: " + Integer.toString(getXCoord()) + 
+            SmartDashboard.putString("Coords", "X: " + Integer.toString(getXCoord()) + 
                                                "   Y: " + Integer.toString(getYCoord()));
         // If the string IS the nopeString
         } else {
